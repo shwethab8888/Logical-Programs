8bd7f03ccb06c78ba23af85a8fb8085757561629
shwethab8888 <shwethab8888@gmail.com>
2023-03-01 21:08:52 +0530

    Fibbonacci Series in logical programs

---
+++ Logical_Programming/FibbonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    public class FibbonacciSeries
+    {
+        public static int PreviousToPrevious = 0;
+        public static int Previous = 0;
+        public static int Next = 1;
+
+        public static void Fibbonacci_Series()
+        {
+            Console.WriteLine("Please Enter The Number To Find Fibonacciseries");
+            int input = Convert.ToInt32(Console.ReadLine());
+            while (Next < input)
+            {
+                PreviousToPrevious = Previous;
+                Previous = Next;
+                Next = PreviousToPrevious + Previous;
+                Console.WriteLine(Previous);
+            }
+        }
+    }
+}
---
+++ Logical_Programming/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Logical_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcomr to logical programs");
+            FibbonacciSeries.Fibbonacci_Series();
+
+
+        }
+    }
+}
